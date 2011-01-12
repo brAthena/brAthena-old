a6626a5784fbc26bb339c49175681f986984a897
Protimus <rafaelgorges@outlook.com>
2011-01-12 00:22:27 +0000

    * Adicionado @go para Cidade de Brasilis. - Não existe no eAthena, não apenas de Brasilis mas de vários novos campos. OBS: Quando as traduções terminarem será enviado massivamente NPCs que não tem no eA, de novos EPs.

--- trunk/src/map/atcommand.c
+++ trunk/src/map/atcommand.c
@@ -2191,6 +2191,7 @@ ACMD_FUNC(go)
 		{ MAP_RACHEL,      130, 110 }, // 23=Rachel
 		{ MAP_VEINS,       216, 123 }, // 24=Veins
 		{ MAP_MOSCOVIA,    223, 184 }, // 25=Moscovia
+		{ MAP_BRASILIS,	   195, 213	}, // 26=Brasilis
 	};
  
 	nullpo_retr(-1, sd);
@@ -2218,7 +2219,7 @@ ACMD_FUNC(go)
 		clif_displaymessage(fd, " 15=Novice Grounds  16=Prison      17=Jawaii");
 		clif_displaymessage(fd, " 18=Ayothaya        19=Einbroch    20=Lighthalzen");
 		clif_displaymessage(fd, " 21=Einbech         22=Hugel       23=Rachel");
-		clif_displaymessage(fd, " 24=Veins           25=Moscovia");
+		clif_displaymessage(fd, " 24=Veins           25=Moscovia	26=Brasilis");
 		return -1;
 	}
 
@@ -2292,6 +2293,8 @@ ACMD_FUNC(go)
 		town = 24;
 	} else if (strncmp(map_name, "moscovia", 3) == 0) {
 		town = 25;
+	} else if (strncmp(map_name, "brasilis", 3) == 0) {
+		town = 26;
 	}
 
 	if (town >= 0 && town < ARRAYLENGTH(data))
