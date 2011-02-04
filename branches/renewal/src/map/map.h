7a8df93cb4d46cead8c5bbbdc94635d37bc15e46
Protimus <rafaelgorges@outlook.com>
2011-02-04 06:26:21 +0000

    * Correções para o sistema de classes. * Quantias máximas alteradas na source. - MAX_SKILL_TREE para 100 - MAX_SKILL para 3000

--- branches/renewal/src/map/map.h
+++ branches/renewal/src/map/map.h
@@ -68,6 +68,8 @@ struct item_data;
 //for filtering and quick checking.
 #define MAPID_UPPERMASK 0x0fff
 #define MAPID_BASEMASK 0x00ff
+#define MAPID_UPPERMASK_THIRD (JOBL_THIRD|MAPID_UPPERMASK)
+#define MAPID_JOBMASK 0xf000
 //First Jobs
 //Note the oddity of the novice:
 //Super Novices are considered the 2-1 version of the novice! Novices are considered a first class type, too...
--- branches/renewal/src/map/pc.h
+++ branches/renewal/src/map/pc.h
@@ -415,7 +415,7 @@ struct map_session_data {
 };
 
 //Update this max as necessary. 54 is the value needed for Super Baby currently
-#define MAX_SKILL_TREE 54
+#define MAX_SKILL_TREE 100
 //Total number of classes (for data storage)
 #define CLASS_COUNT (JOB_MAX - JOB_NOVICE_HIGH + JOB_MAX_BASIC)
 
@@ -539,7 +539,7 @@ extern int duel_count;
 #define pc_check_weapontype(sd, type) ((type)&((sd)->status.weapon < MAX_WEAPON_TYPE? \
 	1<<(sd)->status.weapon:(1<<(sd)->weapontype1)|(1<<(sd)->weapontype2)))
 //Checks if the given class value corresponds to a player class. [Skotlex]
-#define pcdb_checkid(class_) (class_ < JOB_MAX_BASIC || (class_ >= JOB_NOVICE_HIGH && class_ < JOB_MAX))
+#define pcdb_checkid(class_) (class_ < JOB_MAX_BASIC || (class_ >= JOB_NOVICE_HIGH) || (class_ >= JOB_RUNE_KNIGHT && class_ <= JOB_MECHANIC_T2) || (class_ >= JOB_BABY_RUNE && class_ < JOB_MAX))
 
 int pc_class2idx(int class_);
 int pc_isGM(struct map_session_data *sd);
