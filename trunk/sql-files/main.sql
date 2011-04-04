71d9c639ea597174f170a64da3cc824a343992a5
Protimus <rafaelgorges@outlook.com>
2011-04-04 17:13:33 +0000

    * Adicionado novo campo na tabela de login do SQL. - reg_account para datar registro de conta.

--- trunk/sql-files/main.sql
+++ trunk/sql-files/main.sql
@@ -408,6 +408,7 @@ CREATE TABLE IF NOT EXISTS `login` (
   `lastlogin` datetime NOT NULL default '0000-00-00 00:00:00',
   `last_ip` varchar(100) NOT NULL default '',
   `birthdate` DATE NOT NULL DEFAULT '0000-00-00',
+  `reg_account` TIMESTAMP NOT NULL,
   PRIMARY KEY  (`account_id`),
   KEY `name` (`userid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2000000; 
