c63cb98b1c5371038689eb4b4e4677ba714bd772
Protimus <rafaelgorges@outlook.com>
2011-11-09 20:03:39 +0000

    == Renovação == * Correções para o sistema de montarias de aluguel. - Visualização das classes com montarias corrigidas. - Adicionado delay ao item de montarias de alguel.

--- versoes/renovacao/src/map/clif.h
+++ versoes/renovacao/src/map/clif.h
@@ -636,7 +636,7 @@ void clif_open_search_store_info(struct map_session_data* sd);
 void clif_search_store_info_click_ack(struct map_session_data* sd, short x, short y);
 
 void clif_millenniumshield(struct map_session_data *sd, short shields );
-void clif_display_banding(struct block_list *dst, struct block_list *bl, int val1);
+void clif_status_change_single(struct block_list *dst, struct block_list *bl, int type, int flag, unsigned int tick, int val1, int val2, int val3);
 void clif_elemental_info(struct map_session_data *sd);
 void clif_elemental_updatestatus(struct map_session_data *sd, int type);
 
