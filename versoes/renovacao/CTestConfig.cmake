bfbc7ff460df1cf05d0aa005a520ca03ffd935d3
RenatoUtsch <renatoutsch@gmail.com>
2012-03-18 16:26:56 +0000

    == Renovação == * Corrigido parcialmente bug com o CMake. - O MinGW ainda está acusando alguns warnings, mas tudo está compilando normal. - Todos os que usam linux vão ter que baixar a libconfig também para compilar.

--- versoes/renovacao/CTestConfig.cmake
+++ versoes/renovacao/CTestConfig.cmake
@@ -4,12 +4,12 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
-set(CTEST_PROJECT_NAME "eAthena")
+set(CTEST_PROJECT_NAME "brAthena")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.eathena.ws")
-set(CTEST_DROP_LOCATION "/submit.php?project=eAthena")
+set(CTEST_DROP_SITE "cdash.brathena.ws")
+set(CTEST_DROP_LOCATION "/submit.php?project=brAthena")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_USE_LAUNCHERS 1)
