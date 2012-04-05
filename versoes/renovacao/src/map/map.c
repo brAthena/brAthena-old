42fc1fc9ff11b474c9b4b35ac1ae30da758e17fb
RenatoUtsch <renatoutsch@gmail.com>
2012-04-05 03:00:40 +0000

    == Renovação == * Corrigido vazamento de memória que criei sem querer na r1097.

--- versoes/renovacao/src/map/map.c
+++ versoes/renovacao/src/map/map.c
@@ -2453,23 +2453,25 @@ int map_random_dir(struct block_list *bl, short *x, short *y)
 // gatŒn
 inline static struct mapcell map_gat2cell(int gat)
 {
-	struct mapcell *cell = aCalloc(1, sizeof(struct mapcell));
+	struct mapcell cell;
+	
+	memset(&cell, 0, sizeof(struct mapcell));
 	
 	switch( gat )
 	{
-	case 0: cell->walkable = 1; cell->shootable = 1; cell->water = 0; break; // walkable ground
-	case 1: cell->walkable = 0; cell->shootable = 0; cell->water = 0; break; // non-walkable ground
-	case 2: cell->walkable = 1; cell->shootable = 1; cell->water = 0; break; // ???
-	case 3: cell->walkable = 1; cell->shootable = 1; cell->water = 1; break; // walkable water
-	case 4: cell->walkable = 1; cell->shootable = 1; cell->water = 0; break; // ???
-	case 5: cell->walkable = 0; cell->shootable = 1; cell->water = 0; break; // gap (snipable)
-	case 6: cell->walkable = 1; cell->shootable = 1; cell->water = 0; break; // ???
+	case 0: cell.walkable = 1; cell.shootable = 1; cell.water = 0; break; // walkable ground
+	case 1: cell.walkable = 0; cell.shootable = 0; cell.water = 0; break; // non-walkable ground
+	case 2: cell.walkable = 1; cell.shootable = 1; cell.water = 0; break; // ???
+	case 3: cell.walkable = 1; cell.shootable = 1; cell.water = 1; break; // walkable water
+	case 4: cell.walkable = 1; cell.shootable = 1; cell.water = 0; break; // ???
+	case 5: cell.walkable = 0; cell.shootable = 1; cell.water = 0; break; // gap (snipable)
+	case 6: cell.walkable = 1; cell.shootable = 1; cell.water = 0; break; // ???
 	default:
 		ShowWarning("map_gat2cell: gat tipo '%d' nao reconhecido\n", gat);
 		break;
 	}
 
-	return *cell;
+	return cell;
 }
 
 static int map_cell2gat(struct mapcell cell)
