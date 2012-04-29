d0cbd3ddc033d9dec0f020a43980c90162a7f81b
Protimus <rafaelgorges@outlook.com>
2012-04-29 17:27:59 +0000

    * Adicionados: item_avail.sql, improvise_db.sql e magicmushroom_db.sql.

---
+++ versoes/testes/db/re/sql/item_avail.sql
@@ -0,0 +1,29 @@
+--
+-- Estrutura para a tabela `item_avail`
+--         
+
+CREATE TABLE `item_avail` (
+   `ItemID` smallint(6) unsigned NOT NULL default '0',
+   `SpriteID` smallint(6) unsigned NOT NULL default '0',
+   PRIMARY KEY (`SourceID`)
+) ENGINE=MyISAM;
+
+#
+# Aparência de Itens.
+# Descrição: Responsável pela visualização de uma sprite diferente em um item.
+# Cabeçalho: REPLACE INTO `item_avail` VALUES ('ID do Item','ID da Sprite');
+# 
+
+# 'Quest do Bigode Grisalho'
+REPLACE INTO `item_avail` VALUES (2240,2241);
+
+# 'Quest - Caça ao Tesouro de Geffenia'
+#REPLACE INTO `item_avail` VALUES (7950,7021);
+#REPLACE INTO `item_avail` VALUES (7951,7042);
+#REPLACE INTO `item_avail` VALUES (1998,1250);
+#REPLACE INTO `item_avail` VALUES (1999,1472);
+
+# 'Quest - St. Patrics (2008)'
+#REPLACE INTO `item_avail` VALUES (12715,7721);
+#REPLACE INTO `item_avail` VALUES (7915,673);
+#REPLACE INTO `item_avail` VALUES (7916,675);
