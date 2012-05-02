9fb5d34c455988ed9f72617eed9aaa1be9fe8c10
Protimus <rafaelgorges@outlook.com>
2012-05-02 02:23:42 +0000

    * Adicionados: mob_chat_db.sql, mob_penalty_db.sql, mob_race2_db.sql e refine_db.sql.

---
+++ versoes/testes/db/re/sql/mob_race2_db.sql
@@ -0,0 +1,29 @@
+--
+-- Estrutura para a tabela `mob_race2_db`
+-- 
+
+CREATE TABLE `mob_race2_db` (
+  `Race2ID` tinyint(2) unsigned NOT NULL default '0',
+  `MobID1` smallint(6) unsigned NOT NULL default '0',
+  `MobID2` smallint(6) unsigned NOT NULL default '0',
+  `MobID3` smallint(6) unsigned NOT NULL default '0',
+  `MobID4` smallint(6) unsigned NOT NULL default '0',
+  `MobID5` smallint(6) unsigned NOT NULL default '0',
+  `MobID6` smallint(6) unsigned NOT NULL default '0',
+  `MobID7` smallint(6) unsigned NOT NULL default '0',
+  `MobID8` smallint(6) unsigned NOT NULL default '0',
+  `MobID9` smallint(6) unsigned NOT NULL default '0',
+  PRIMARY KEY (`Race2ID`)	
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Raça dos monstros.
+# Descrição: Responsável pelo banco de dados de raça dos monstros.
+# Cabeçalho: REPLACE INTO `mob_race2_db` VALUES ('ID da Raça,ID do Mob 1,ID do Mob2, etc...');
+
+REPLACE INTO 'mob_race2_db' VALUES (1,1122,1123,1124,1125,1126,1245,1258,1280,1299,1308,1607);	#'Goblins'
+REPLACE INTO 'mob_race2_db' VALUES (2,1133,1134,1135,1282,1296);	#'Kobolds'
+REPLACE INTO 'mob_race2_db' VALUES (3,1023,1152,1153,1189,1213,1273,1981,1982,1983,1984);	#'Orcs'
+REPLACE INTO 'mob_race2_db' VALUES (4,1040,1278,1366,1497,2024);	#'Golens'
+REPLACE INTO 'mob_race2_db' VALUES (5,1285,1286,1287,1829,1830,1899,1907,1908,1949,1950);	#'Guardiões'
+REPLACE INTO 'mob_race2_db' VALUES (6,1315,1364,1401,1560);	#'Ninjas'
