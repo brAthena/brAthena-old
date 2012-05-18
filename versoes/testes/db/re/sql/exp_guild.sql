b9671835c5f4dc4348b34f91d3bde1cd5d0d9783
Protimus <rafaelgorges@outlook.com>
2012-05-18 22:39:47 +0000

    * Adicionados: attr_fix.sql, exp_guild.sql, exp_homun.sql.

---
+++ versoes/testes/db/re/sql/exp_guild.sql
@@ -0,0 +1,63 @@
+--
+-- Estrutura para a tabela `exp_guild`
+--    
+ 
+CREATE TABLE `exp_guild` (
+	`EXP` int(11) NOT NULL default '0',
+	PRIMARY KEY (`EXP`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Experiência da guilda.
+# Descrição: Responsável pela experiência ganha pela guilda.
+# Cabeçalho: REPLACE INTO `exp_guild` VALUES ('Experiência');
+#
+
+REPLACE INTO exp_guild VALUES (400000);	# 'Nível: 01 -> 02'
+REPLACE INTO exp_guild VALUES (900000);	# 'Nível: 02 -> 03'
+REPLACE INTO exp_guild VALUES (1600000);	# 'Nível: 03 -> 04'
+REPLACE INTO exp_guild VALUES (2500000);	# 'Nível: 04 -> 05'
+REPLACE INTO exp_guild VALUES (3600000);	# 'Nível: 05 -> 06'
+REPLACE INTO exp_guild VALUES (4900000);	# 'Nível: 06 -> 07'
+REPLACE INTO exp_guild VALUES (6400000);	# 'Nível: 07 -> 08'
+REPLACE INTO exp_guild VALUES (8100000);	# 'Nível: 08 -> 09'
+REPLACE INTO exp_guild VALUES (10000000);	# 'Nível: 09 -> 10'
+REPLACE INTO exp_guild VALUES (12100000);	# 'Nível: 10 -> 11'
+REPLACE INTO exp_guild VALUES (14400000);	# 'Nível: 11 -> 12'
+REPLACE INTO exp_guild VALUES (16900000);	# 'Nível: 12 -> 13'
+REPLACE INTO exp_guild VALUES (19600000);	# 'Nível: 13 -> 14'
+REPLACE INTO exp_guild VALUES (22500000);	# 'Nível: 14 -> 15'
+REPLACE INTO exp_guild VALUES (25600000);	# 'Nível: 15 -> 16'
+REPLACE INTO exp_guild VALUES (28900000);	# 'Nível: 16 -> 17'
+REPLACE INTO exp_guild VALUES (32400000);	# 'Nível: 17 -> 18'
+REPLACE INTO exp_guild VALUES (36100000);	# 'Nível: 18 -> 19'
+REPLACE INTO exp_guild VALUES (40000000);	# 'Nível: 19 -> 20'
+REPLACE INTO exp_guild VALUES (44100000);	# 'Nível: 20 -> 21'
+REPLACE INTO exp_guild VALUES (48400000);	# 'Nível: 21 -> 22'
+REPLACE INTO exp_guild VALUES (52900000);	# 'Nível: 22 -> 23'
+REPLACE INTO exp_guild VALUES (57600000);	# 'Nível: 23 -> 24'
+REPLACE INTO exp_guild VALUES (62500000);	# 'Nível: 24 -> 25'
+REPLACE INTO exp_guild VALUES (67600000);	# 'Nível: 25 -> 26'
+REPLACE INTO exp_guild VALUES (72900000);	# 'Nível: 26 -> 27'
+REPLACE INTO exp_guild VALUES (78400000);	# 'Nível: 27 -> 28'
+REPLACE INTO exp_guild VALUES (84100000);	# 'Nível: 28 -> 29'
+REPLACE INTO exp_guild VALUES (90000000);	# 'Nível: 29 -> 30'
+REPLACE INTO exp_guild VALUES (96100000);	# 'Nível: 30 -> 31'
+REPLACE INTO exp_guild VALUES (102400000);	# 'Nível: 31 -> 32'
+REPLACE INTO exp_guild VALUES (108900000);	# 'Nível: 32 -> 33'
+REPLACE INTO exp_guild VALUES (115600000);	# 'Nível: 33 -> 34'
+REPLACE INTO exp_guild VALUES (122500000);	# 'Nível: 34 -> 35'
+REPLACE INTO exp_guild VALUES (129600000);	# 'Nível: 35 -> 36'
+REPLACE INTO exp_guild VALUES (136900000);	# 'Nível: 36 -> 37'
+REPLACE INTO exp_guild VALUES (144400000);	# 'Nível: 37 -> 38'
+REPLACE INTO exp_guild VALUES (152100000);	# 'Nível: 38 -> 39'
+REPLACE INTO exp_guild VALUES (160000000);	# 'Nível: 39 -> 40'
+REPLACE INTO exp_guild VALUES (168100000);	# 'Nível: 40 -> 41'
+REPLACE INTO exp_guild VALUES (176400000);	# 'Nível: 41 -> 42'
+REPLACE INTO exp_guild VALUES (184900000);	# 'Nível: 42 -> 43'
+REPLACE INTO exp_guild VALUES (193600000);	# 'Nível: 44 -> 45'
+REPLACE INTO exp_guild VALUES (211600000);	# 'Nível: 45 -> 46'
+REPLACE INTO exp_guild VALUES (220900000);	# 'Nível: 46 -> 47'
+REPLACE INTO exp_guild VALUES (230400000);	# 'Nível: 47 -> 48'
+REPLACE INTO exp_guild VALUES (240100000);	# 'Nível: 48 -> 49'
+REPLACE INTO exp_guild VALUES (250000000);	# 'Nível: 49 -> 50'
