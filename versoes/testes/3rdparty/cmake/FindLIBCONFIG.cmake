46036f334bc34f7dbf51f38c96ad26f9533c4e18
RenatoUtsch <renatoutsch@gmail.com>
2012-05-19 21:12:57 +0000

    == Testes == * Removido suporte ao autoconf. * Reescrito suporte ao CMake. - Logo teremos suporte ao CTest e CDash também.

--- versoes/testes/3rdparty/cmake/FindLIBCONFIG.cmake
+++ versoes/testes/3rdparty/cmake/FindLIBCONFIG.cmake
@@ -6,34 +6,18 @@
 #  LIBCONFIG_FOUND        - True if libconfig found.
 
 
-find_path( LIBCONFIG_INCLUDE_DIR libconfig.h
-	PATHS "/usr/include"
-		  "/usr/local/include" )
-		
-find_library( LIBCONFIG_LIBRARY NAMES libconfig config
-	PATHS "/usr/lib"
-		  "/usr/local/lib" )
+find_path( LIBCONFIG_INCLUDE_DIR libconfig.h )
+
+find_library( LIBCONFIG_LIBRARY NAMES libconfig config )
 
 mark_as_advanced( LIBCONFIG_LIBRARY LIBCONFIG_INCLUDE_DIR )
 
-if( LIBCONFIG_INCLUDE_DIR AND EXISTS "${LIBCONFIG_INCLUDE_DIR}/libconfig.h" )
-	file( STRINGS "${LIBCONFIG_INCLUDE_DIR}/libconfig.h" LIBCONFIG_H REGEX "^#define[ \t]+LIBCONFIG_VER_M[A-Z]+[ \t]+[0-9]+.*$" )
-	string( REGEX REPLACE "^.*LIBCONFIG_VER_MAJOR[ \t]+([0-9]+).*$" "\\1" LIBCONFIG_MAJOR "${LIBCONFIG_H}" )
-	string( REGEX REPLACE "^.*LIBCONFIG_VER_MINOR[ \t]+([0-9]+).*$" "\\1" LIBCONFIG_MINOR  "${LIBCONFIG_H}" )
-
-	set( LIBCONFIG_VERSION_STRING "${LIBCONFIG_MAJOR}.${LIBCONFIG_MINOR}" )
-	set( LIBCONFIG_VERSION_MAJOR "${LIBCONFIG_MAJOR}" )
-	set( LIBCONFIG_VERSION_MINOR "${LIBCONFIG_MINOR}" )
-endif()
-
-# handle the QUIETLY and REQUIRED arguments and set LIBCONFIG_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set LIBCONFIG_FOUND to TRUE if
 # all listed variables are TRUE
 include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( LIBCONFIG
-	DEFAULT_MSG LIBCONFIG_LIBRARY LIBCONFIG_INCLUDE_DIR
-)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( LIBCONFIG DEFAULT_MSG LIBCONFIG_LIBRARY LIBCONFIG_INCLUDE_DIR )
 
 if( LIBCONFIG_FOUND )
-	set( LIBCONFIG_LIBRARIES ${LIBCONFIG_LIBRARY} )
 	set( LIBCONFIG_INCLUDE_DIRS ${LIBCONFIG_INCLUDE_DIR} )
-endif()
+	set( LIBCONFIG_LIBRARIES ${LIBCONFIG_LIBRARY} )
+endif( LIBCONFIG_FOUND )
--- versoes/testes/3rdparty/cmake/FindMYSQL.cmake
+++ versoes/testes/3rdparty/cmake/FindMYSQL.cmake
@@ -1,37 +1,57 @@
-# - Find mysqlclient
+# - Find mysql
 # Find the native MySQL includes and library
 #
-#  MYSQL_INCLUDE_DIRS - where to find mysql.h, etc.
-#  MYSQL_LIBRARIES   - mysqlclient library.
-#  MYSQL_FOUND       - True if mysqlclient is found.
-#
+#  MYSQL_INCLUDE_DIRS - where to find mysql.h.
+#  MYSQL_LIBRARIES    - List of libraries when using mysql.
+#  MYSQL_FOUND        - True if mysql found.
 
-find_path( MYSQL_INCLUDE_DIRS "mysql.h"
-	PATHS
-		"/usr/include/mysql"
-		"/usr/local/include/mysql"
-		"/usr/mysql/include/mysql"
-		"$ENV{PROGRAMFILES}/MySQL/*/include"
-		"$ENV{SYSTEMDRIVE}/MySQL/*/include" )
+if( WIN32 )
+	find_path( MYSQL_INCLUDE_DIR mysql.h
+		PATHS
+			"$ENV{PROGRAMFILES}/MySQL/include"
+			"$ENV{SYSTEMDRIVE}/MySQL/include"
+			"$ENV{PROGRAMFILES}/MySQL/*/include"
+			"$ENV{SYSTEMDRIVE}/MySQL/*/include" )
+	find_library( MYSQL_LIBRARY
+		NAMES
+			mysqlclient mysqlclient_r mysql libmysql
+		PATHS
+			"$ENV{PROGRAMFILES}/MySQL/lib"
+			"$ENV{PROGRAMFILES}/MySQL/lib64"
+			"$ENV{PROGRAMFILES}/MySQL/*/lib"
+			"$ENV{PROGRAMFILES}/MySQL/*/lib64"
+			"$ENV{SYSTEMDRIVE}/MySQL/lib"
+			"$ENV{SYSTEMDRIVE}/MySQL/lib64"
+			"$ENV{SYSTEMDRIVE}/MySQL/*/lib"
+			"$ENV{SYSTEMDRIVE}/MySQL/*/lib64" )
+else( WIN32 )
+	find_path( MYSQL_INCLUDE_DIR mysql.h
+		PATHS
+			"/usr/include/mysql"
+			"/usr/local/include/mysql"
+			"/usr/mysql/include"
+			"/usr/mysql/include/mysql" )
+	find_library( MYSQL_LIBRARY
+		NAMES
+			mysqlclient mysqlclient_r mysql libmysql
+		PATHS
+			"/usr/lib/mysql"
+			"/usr/lib64/mysql"
+			"/usr/local/lib/mysql"
+			"/usr/local/lib64/mysql"
+			"/usr/mysql/lib"
+			"/usr/mysql/lib/mysql"
+			"/usr/mysql/lib64"
+			"/usr/mysql/lib64/mysql" )
+endif( WIN32 )
+mark_as_advanced( MYSQL_INCLUDE_DIR MYSQL_LIBRARY )
 
-find_library( MYSQL_LIBRARIES
-	NAMES "mysqlclient" "mysqlclient_r" "mysql" "libmysql"
-	PATHS
-		"/usr/lib/mysql"
-		"/usr/local/lib/mysql"
-		"/usr/mysql/lib/mysql"
-		"$ENV{PROGRAMFILES}/MySQL/*/lib"
-		"$ENV{SYSTEMDRIVE}/MySQL/*/lib" )
-mark_as_advanced( MYSQL_LIBRARIES  MYSQL_INCLUDE_DIRS )
-
-if( MYSQL_INCLUDE_DIRS AND EXISTS "${MYSQL_INCLUDE_DIRS}/mysql_version.h" )
-	file( STRINGS "${MYSQL_INCLUDE_DIRS}/mysql_version.h" MYSQL_VERSION_H REGEX "^#define[ \t]+MYSQL_SERVER_VERSION[ \t]+\"[^\"]+\".*$" )
-	string( REGEX REPLACE "^.*MYSQL_SERVER_VERSION[ \t]+\"([^\"]+)\".*$" "\\1" MYSQL_VERSION_STRING "${MYSQL_VERSION_H}" )
-endif()
-
-# handle the QUIETLY and REQUIRED arguments and set MYSQL_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set MYSQL_FOUND to TRUE if
 # all listed variables are TRUE
 include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( MYSQL
-	DEFAULT_MSG MYSQL_LIBRARIES MYSQL_INCLUDE_DIRS
-)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( MYSQL DEFAULT_MSG MYSQL_INCLUDE_DIR MYSQL_LIBRARY )
+
+if( MYSQL_FOUND )
+	set( MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIR} )
+	set( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
+endif( MYSQL_FOUND )
