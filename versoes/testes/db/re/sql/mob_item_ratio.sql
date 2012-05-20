120a3f8d158542a2736c28dd39a45e0eda25311f
Protimus <rafaelgorges@outlook.com>
2012-05-20 16:45:25 +0000

    * Adicionados: mob_branch.sql, mob_avail.sql e mob_item_ratio.sql.

---
+++ versoes/testes/db/re/sql/mob_item_ratio.sql
@@ -0,0 +1,15 @@
+--
+-- Estrutura para a tabela `mob_item_ratio`
+-- 
+
+CREATE TABLE IF NOT EXISTS `mob_item_ratio` (
+  `ItemID` smallint(6) NOT NULL default '0',
+  `Rate` int(11) NOT NULL default '0',
+  `MobID` smallint(6) NOT NULL default '0',
+  PRIMARY KEY (`ItemID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+#
+# Queda de itens.
+# Descrição: Taxa de queda de itens especifica para determinados monstros e condições.
+# Cabeçalho: REPLACE INTO `mob_penalty_db` VALUES ('ItemID,Rate,MobID');
