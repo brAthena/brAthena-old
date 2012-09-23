57a14a972a6c2b3f6a59653c4ddd09c28b7d38db
Protimus <rafaelgorges@outlook.com>
2012-09-23 06:03:50 +0000

    Removidas todas as versões antigas, para adição da nova versão oficial.

--- versoes/pre-renovacao/3rdparty/cmake/monotonic_clock_check.c
+++
@@ -1,8 +0,0 @@
-#include <sys/time.h>
-#include <time.h>
-#include <unistd.h>
-
-int main(void) {
-	struct timespec tval;
-	return clock_gettime(CLOCK_MONOTONIC, &tval);
-}
--- versoes/pre-renovacao/sql-files/conversor_senhas.sql
+++
@@ -1,3 +0,0 @@
-# Converte as senhas para MD5 'hashes'
-
-UPDATE `login` SET `user_pass`=MD5(`user_pass`);
