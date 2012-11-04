e020b611ab019cf4f754e17c1bc61c98597309e0
Megasantos <jonataandretta@hotmail.com>
2012-11-04 02:04:26 +0000

    * Adicionado atualizações da r1413.

---
+++ testes/db/pt-BR/updates/brathena_svn1413.sql
@@ -0,0 +1,18 @@
+/*_________________________________________________________________________
+ /                                                                         \
+ |                  _           _   _   _                                  |
+ |                 | |__  _ __ / \ | |_| |__   ___ _ __   __ _             |
+ |                 | '_ \| '__/ _ \| __| '_ \ / _ \ '_ \ / _` |            |
+ |                 | |_) | | / ___ \ |_| | | |  __/ | | | (_| |            |
+ |                 |_.__/|_|/_/   \_\__|_| |_|\___|_| |_|\__,_|            |
+ |                                                                         |
+ |                       brAthena © 2012 - Banco de Dados                  |
+ |             Contém as atualizações de habilidades, itens e monstros     |
+ \_________________________________________________________________________/
+*/
+
+--
+-- Atualização da tabela `skill_db`
+--
+REPLACE INTO `skill_db` VALUES(378, '0', 6, 4, '5', '0x1', '0', 5, '1', 'no', 0, '0', '0', 'weapon', 0, '		ASC_EDP', 'Encantar com Veneno Mortal\r\n');
+REPLACE INTO `skill_db` VALUES(379, '7', 6, 1, '-1', '0x8', '0', 10, '1', 'yes', 0, '0', '0', 'weapon', 0, '	ASC_BREAKER', 'Destruidor de Almas\r\n');
