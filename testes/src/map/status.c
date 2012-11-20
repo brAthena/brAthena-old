43f9d718e07204925e0efa93713f4ca1dfdaaaad
Megasantos <jonataandretta@hotmail.com>
2012-11-20 02:54:00 +0000

    * Correção da r1442.

--- testes/src/map/status.c
+++ testes/src/map/status.c
@@ -1044,6 +1044,7 @@ void initChangeTables(void) {
 	StatusChangeStateTable[SC_DEEPSLEEP]           |= SCS_NOCAST;
 	StatusChangeStateTable[SC_SATURDAYNIGHTFEVER]  |= SCS_NOCAST;
 	StatusChangeStateTable[SC_CURSEDCIRCLE_TARGET] |= SCS_NOCAST;
+	StatusChangeStateTable[SC_SILENCE]             |= SCS_NOCAST;
 	
 	//Homon S
 	StatusChangeStateTable[SC_PARALYSIS]               |= SCS_NOMOVE;
