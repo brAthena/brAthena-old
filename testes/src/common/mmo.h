ff274178f7c2ee2d1a4885c9b6e1e0cf18fafef8
Megasantos <jonataandretta@hotmail.com>
2013-04-27 17:58:19 +0000

    * Remoção de PACKETVER desnecessário.

--- testes/src/common/mmo.h
+++ testes/src/common/mmo.h
@@ -60,8 +60,7 @@
 // 20120307 - 2012-03-07aRagexeRE+ - 0x970
 
 #ifndef PACKETVER
-#define PACKETVER 20120410
-//#define PACKETVER 20111116
+	#define PACKETVER 20120410
 #endif
 
 //Remove/Comment this line to disable sc_data saving. [Skotlex]
