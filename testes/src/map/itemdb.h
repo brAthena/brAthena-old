af361cc08fc9860374db47fcfae2a703aa300167
Holy <teste@brathena.org>
2013-06-09 06:11:52 +0000

    * Revisão e atualização ( Ovos bRO ). - Expansão do limite máximo de grupos (140). - Correção em exibição de cutin ( npc/instancias/santuario_selado.txt ). - Complemento do envio 2185.

--- testes/src/map/itemdb.h
+++ testes/src/map/itemdb.h
@@ -91,7 +91,7 @@ enum {
 #define IG_FINDINGORE 6
 #define IG_POTION 37
 //The max. item group count (increase this when needed).
-#define MAX_ITEMGROUP 107
+#define MAX_ITEMGROUP 140
 
 #define CARD0_FORGE 0x00FF
 #define CARD0_CREATE 0x00FE
