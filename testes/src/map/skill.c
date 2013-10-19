d6b423dcaf500f3afaecb9586d0d9fd7fe1f5679
Protimus <rafaelgorges@outlook.com>
2013-10-19 23:36:07 +0000

    * Atualização da r2470. - Não há necessidade do uso de checagem da habilidade MANHOLE para a condição de versão oldtimes em skill.c.

--- testes/src/map/skill.c
+++ testes/src/map/skill.c
@@ -11746,7 +11746,7 @@ int skill_unit_onplace_timer(struct skill_unit *src, struct block_list *bl, unsi
 #if VERSION != -1
 				if(sg->unit_id == UNT_MANHOLE || battle_config.skill_trap_type || !map_flag_gvg2(src->bl.m)) {
 #else
-				if(sg->unit_id == UNT_MANHOLE || battle_config.skill_trap_type_ot || !map_flag_gvg2(src->bl.m)) {
+				if(battle_config.skill_trap_type_ot || !map_flag_gvg2(src->bl.m)) {
 #endif
 					unit_movepos(bl, src->bl.x, src->bl.y, 0, 0);
 					clif_fixpos(bl);
